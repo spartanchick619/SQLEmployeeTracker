f6912adb7b50520d14cb62781b1d824a7adfb69d
Rosa Valdez <rosa.m42@icloud.com>
2023-12-07 20:30:02 -0600

    added db,table

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR (40) NOT NULL
+);
+
+CREATE TABLE roles(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30)
+  salary DECIMAL(10,2),
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL 
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR (30) NOT NULL,
+  last_name VARCHAR (30) NOT NULL,
+  role_id INT,
+  manager_id INT NOT NULL
+);
